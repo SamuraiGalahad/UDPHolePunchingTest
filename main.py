9360dbc0da93e304350003a5e54267e1877b691d
Parakhin Nikolay <66258171+SamuraiGalahad@users.noreply.github.com>
2025-03-18 14:21:25 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import random
+import socket
+import threading
+import time
+
+SERVER_IP = "ip"
+SERVER_PORT = 9999
+LOCAL_PORT = 6000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.bind(("0.0.0.0", LOCAL_PORT))
+
+code = "TESTCODE"
+
+client.sendto(code.encode(), (SERVER_IP, SERVER_PORT))
+
+peers = set()
+
+
+def receive_messages():
+    while True:
+        data, addr = client.recvfrom(1024)
+        message = data.decode()
+        print(message)
+        if ":" in message:
+            peer_addr = (message.split(":")[0], int(message.split(":")[1]))
+            peers.add(peer_addr)
+
+            for _ in range(5):
+                client.sendto(b"Punch", peer_addr)
+
+            print(f"{peer_addr}")
+
+
+threading.Thread(target=receive_messages, daemon=True).start()
+msg = random.randint(1, 10)
+while True:
+    if msg:
+        for peer in peers:
+            client.sendto(str(msg).encode(), peer)
+            time.sleep(0.5)
